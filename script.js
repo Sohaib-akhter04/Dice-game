6bb2be36dc8bbcbc10d3b311c367966a8de79d5c
sohaib <akhtersohaib56@gmail.com>
2023-04-21 05:09:34 +0500

    seems like final one

--- script.js
+++ script.js
@@ -13,18 +13,24 @@ let winner=(randomNumber1,randomNumber2)=>{
 
 
 reset.addEventListener('click', () => {
-    var randomNumber1 = Math.floor(Math.random() * 6) + 1
-    var randdiceimg = "dice" + randomNumber1 + ".png";
-    var randimgsrc1 = "images/" + randdiceimg;
+    setTimeout(function(){
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1
+        var randdiceimg = "dice" + randomNumber1 + ".png";
+        var randimgsrc1 = "images/" + randdiceimg;
+    
+        document.querySelectorAll("img")[0].setAttribute("src", randimgsrc1);
+    
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1
+        var randdiceimg = "dice" + randomNumber2 + ".png";
+        var randimgsrc2 = "images/" + randdiceimg;
+        document.querySelectorAll("img")[1].setAttribute("src", randimgsrc2);
+        winner(randomNumber1,randomNumber2)
+        document.querySelector("#reset").innerHTML="Play Again"
+    },500)
 
-    document.querySelectorAll("img")[0].setAttribute("src", randimgsrc1);
-
-    var randomNumber2 = Math.floor(Math.random() * 6) + 1
-    var randdiceimg = "dice" + randomNumber2 + ".png";
-    var randimgsrc2 = "images/" + randdiceimg;
-    document.querySelectorAll("img")[1].setAttribute("src", randimgsrc2);
-    winner(randomNumber1,randomNumber2)
-    document.querySelector("#reset").innerHTML="Play Again"
+    const myElement = document.getElementById('myElement');
+myElement.style.transition = 'transform 1s ease-in-out';
+ 
 
    
 })
